44fb9cd0c831e5813d86f40ca9fc8f4fc6d2710d
osm-pixle <ousamaeddahbir@gmail.com>
2024-07-10 02:44:56 +0100

    c03

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,6 +1,18 @@
-unsigned int		ft_strlen(char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oued-dah <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 02:42:29 by oued-dah          #+#    #+#             */
+/*   Updated: 2024/07/10 02:43:21 by oued-dah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
 	while (str[i])
@@ -8,12 +20,12 @@ unsigned int		ft_strlen(char *str)
 	return (i);
 }
 
-unsigned int		ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int i;
-	unsigned int j;
-	unsigned int res_d;
-	unsigned int res_s;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	res_d;
+	unsigned int	res_s;
 
 	i = ft_strlen(dest);
 	j = 0;
